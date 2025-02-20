8575d15df58839787ccf5e240fd2075d774823a6
narumi <sychoi2008@sookmyung.ac.kr>
2025-02-20 22:31:58 +0900

    swagger error 발생

--- src/main/java/com/sy/review/api/controller/drama/CreateDramaReviewController.java
+++ src/main/java/com/sy/review/api/controller/drama/CreateDramaReviewController.java
@@ -4,19 +4,22 @@ import com.sy.review.api.controller.drama.dto.CreateDramaReviewRequest;
 import com.sy.review.api.service.drama.CreateDramaReviewService;
 import com.sy.review.api.service.drama.dto.CreateDramaReviewCommand;
 import com.sy.review.domain.AuthUtil;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
+@Tag(name = "Create Drama Review", description = "드라마 리뷰 생성")
 @RestController
 @RequestMapping("/api/drama/review")
 public class CreateDramaReviewController {
     private final CreateDramaReviewService createDramaReviewService;
 
     @PostMapping
-    public void createDramaReview(CreateDramaReviewRequest request) {
+    public void createDramaReview(@RequestBody CreateDramaReviewRequest request) {
         createDramaReviewService.execute(
                 CreateDramaReviewCommand.builder()
                         .title(request.title())

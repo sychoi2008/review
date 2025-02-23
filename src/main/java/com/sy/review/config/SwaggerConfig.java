8ac01afdd66fbf9fa3787889da0ff3bd3a94bcfa
narumi <sychoi2008@sookmyung.ac.kr>
2025-02-23 20:34:38 +0900

    swagger 수정

---
+++ src/main/java/com/sy/review/config/SwaggerConfig.java
@@ -0,0 +1,23 @@
+package com.sy.review.config;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .info(apiInfo());
+    }
+
+    private Info apiInfo() {
+        return new Info()
+                .title("Drama Review Swagger")
+                .description("드라마 리뷰 API REST API")
+                .version("1.0.0");
+    }
+}
